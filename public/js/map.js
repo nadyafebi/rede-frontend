cdf86b8969ba5961334945bdc422ae29bf93defb
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2017-10-07 02:16:28 -0700

    Change map location and controls

--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,11 @@ var map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 17.026782, lng: 9.378825},
+    zoom: 3,
+
+    mapTypeControl: false,
+    streetViewControl: false,
+    fullscreenControl: false
   });
 }
