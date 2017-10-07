efdefd5a974b00b6a1605785e64452faad05ef8f
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2017-10-07 04:22:17 -0700

    Update map with highlight and info window

--- public/js/map.js
+++ public/js/map.js
@@ -8,16 +8,45 @@ function initMap() {
   });
 
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 17.026782, lng: 9.378825},
+    center: {lat: 30, lng: 0},
     zoom: Math.ceil(Math.log2($(window).width())) - 8,
 
     mapTypeControl: false,
     streetViewControl: false,
     fullscreenControl: false
   });
+
+  var world_geometry = new google.maps.FusionTablesLayer({
+    query: {
+      select: 'geometry',
+      from: '1N2LBk4JHwWpOY4d9fobIn27lfnZ5MDy-NoqqRpk',
+      where: "ISO_2DIGIT IN ('RU', 'KZ', 'KG')"
+    },
+    styles: [{
+      polygonOptions: {
+        fillColor: '#00ff00',
+        fillOpacity: 0.25
+      }
+    }],
+    map: map,
+    suppressInfoWindows: true
+  });
+
+  createWindow();
 }
 
 function styleMap() {
   map.mapTypes.set('styled_map', styledMapType);
   map.setMapTypeId('styled_map');
+}
+
+function createWindow() {
+  var contentString = 'Здравствуйте';
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: contentString,
+    position: {lat: 61.495, lng: 104.98315}
+  });
+
+  infoWindow.open(map);
 }
