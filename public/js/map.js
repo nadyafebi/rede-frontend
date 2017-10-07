848dcd7b68f04e04cd4608e30688a3bc91a65f7b
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2017-10-07 10:10:31 -0700

    Add input handling

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,15 @@
+// Map-related Variables
 var map;
 var styledMapType;
 
+// Input-related variables
+var inputTextBox = $('#text-box')[0];
+var inputLangBox = $('#lang-box')[0];
+var inputButton = $('#submit-button')[0];
+var inputText;
+var inputLang;
+
+// Map creation
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 30, lng: 0},
@@ -15,23 +24,24 @@ function initMap() {
 
   $.getJSON("js/mapstyle.json", function(data) {
     styledMapType = new google.maps.StyledMapType(data);
-    styleMap();
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
   });
+}
 
-  translate();
+// Input handling
+inputButton.addEventListener('click', function() {
+  inputText = inputTextBox.innerHTML;
+  inputLang = inputLangBox.innerHTML;
 
+  translate(inputText, inputLang);
   highlightMap();
-
   createWindow();
-}
+});
 
-function styleMap() {
-  map.mapTypes.set('styled_map', styledMapType);
-  map.setMapTypeId('styled_map');
-}
-
-function translate() {
-  $.get("https://crossorigin.me/https://rede-182207.appspot.com?lang=en&text=Hello", function(data) {
+function translate(text, lang) {
+  var url = "https://rede-182207.appspot.com/?lang=" + lang + "&text=" + text;
+  $.get(url, function(data) {
     console.log(data);
   });
 }
