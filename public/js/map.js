6583e703d28a931d17c6bc4ea0513b58bad7757b
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2017-10-07 03:27:55 -0700

    Add style to the map

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,12 @@
 var map;
+var styledMapType;
 
 function initMap() {
+  $.getJSON("js/mapstyle.json", function(data) {
+    styledMapType = new google.maps.StyledMapType(data);
+    styleMap();
+  });
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 17.026782, lng: 9.378825},
     zoom: Math.ceil(Math.log2($(window).width())) - 8,
@@ -9,4 +15,9 @@ function initMap() {
     streetViewControl: false,
     fullscreenControl: false
   });
+}
+
+function styleMap() {
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
 }
