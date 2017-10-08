9888f94013dafb3362b50746c7152d920fb244c4
apoorva <anagaraj@trifacta.com>
2017-10-08 01:37:29 -0700

    speech output

--- public/js/app.js
+++ public/js/app.js
@@ -15,6 +15,194 @@ var isInput = false;
 var inputText;
 var inputLang;
 var langData;
+var hover_lang;
+
+var jsonObj= {
+"ab" : "US English Female",
+"aa" : "US English Female",
+"af" : "Afrikaans Male",
+"ak" : "US English Female",
+"sq" : "Albanian Male",
+"am" : "US English Female",
+"ar" : "Arabic Male",
+"an" : "Spanish Female",
+"hy" : "Armenian Male",
+"as" : "Hindi Female",
+"av" : "US English Female",
+"ae" : "Arabic Male",
+"ay" : "Spanish Female",
+"az" : "Turkish Female",
+"bm" : "US English Female",
+"ba" : "Turkish Female",
+"eu" : "US English Female",
+"be" : "Russian Female",
+"bn" : "Hindi Female",
+"bh" : "Hindi Female",
+"bi" : "US English Female",
+"bs" : "Bosnian Male",
+"br" : "French Female",
+"bg" : "Slovak Male",
+"my" : "Chinese Female",
+"ca" : "Catalan Male",
+"ch" : "US English Female",
+"ce" : "Russian Female",
+"ny" : "US English Female",
+"zh" : "Chinese Female",
+"cv" : "Turkish Female",
+"kw" : "French Female",
+"co" : "Romanian Male",
+"cr" : "US English Female",
+"hr" : "Croatian Male",
+"cs" : "Czech Male",
+"da" : "Danish Male",
+"dv" : "US English Female",
+"nl" : "Dutch Female",
+"dz" : "Chinese Female",
+"en" : "US English Female",
+"eo" : "Esperanto Male",
+"et" : "Swedish Female",
+"ee" : "US English Female",
+"fo" : "Danish Male",
+"fj" : "US English Female",
+"fi" : "Finnish Male",
+"fr" : "French Female",
+"ff" : "US English Female",
+"gl" : "Portuguese Female",
+"ka" : "US English Female",
+"de" : "Italian Female",
+"el" : "Greek Female",
+"gn" : "Latin Female",
+"gu" : "Hindi Female",
+"ht" : "Portuguese Female",
+"ha" : "US English Female",
+"he" : "US English Female",
+"hz" : "US English Female",
+"hi" : "Hindi Female",
+"ho" : "US English Female",
+"hu" : "Hungarian Female",
+"ia" : "US English Female",
+"id" : "US English Female",
+"ie" : "US English Female",
+"ga" : "US English Female",
+"ig" : "US English Female",
+"ik" : "US English Female",
+"io" : "US English Female",
+"is" : "Icelandic Male",
+"it" : "Italian Female",
+"iu" : "US English Female",
+"ja" : "Japanese Female",
+"jv" : "US English Female",
+"kl" : "US English Female",
+"kn" : "Tamil Male",
+"kr" : "Arabic Male",
+"ks" : "Hindi Female",
+"kk" : "Turkish Female",
+"km" : "Vietnamese Male",
+"ki" : "US English Female",
+"rw" : "US English Female",
+"ky" : "Turkish Female",
+"kv" : "Russian Female",
+"kg" : "US English Female",
+"ko" : "Korean Female",
+"ku" : "US English Female",
+"kj" : "US English Female",
+"la" : "Latin Female",
+"lb" : "French Female",
+"lg" : "US English Female",
+"li" : "Dutch Female",
+"ln" : "US English Female",
+"lo" : "Thai Female",
+"lt" : "Slovak Male",
+"lu" : "US English Female",
+"lv" : "Latvian Male",
+"gv" : "US English Female",
+"mk" : "Macedonian Male",
+"mg" : "US English Female",
+"ms" : "US English Female",
+"ml" : "Tamil Male",
+"mt" : "US English Female",
+"mi" : "US English Female",
+"mr" : "Hindi Female",
+"mh" : "US English Female",
+"mn" : "Chinese Female",
+"na" : "US English Female",
+"nv" : "US English Female",
+"nd" : "US English Female",
+"ne" : "Hindi Female",
+"ng" : "US English Female",
+"nb" : "Norwegian Male",
+"nn" : "Norwegian Male",
+"no" : "Norwegian Male",
+"ii" : "Chinese Female",
+"nr" : "US English Female",
+"oc" : "US English Female",
+"oj" : "US English Female",
+"cu" : "Slovak Male",
+"om" : "US English Female",
+"or" : "US English Female",
+"os" : "US English Female",
+"pa" : "Hindi Female",
+"pi" : "Arabic Male",
+"fa" : "Arabic Male",
+"pl" : "Polish Female",
+"ps" : "US English Female",
+"pt" : "Portuguese Female",
+"qu" : "US English Female",
+"rm" : "Romanian Male",
+"rn" : "US English Female",
+"ro" : "Romanian Male",
+"ru" : "Russian Female",
+"sa" : "Hindi Female",
+"sc" : "US English Female",
+"sd" : "Hindi Female",
+"se" : "Finnish Male",
+"sm" : "US English Female",
+"sg" : "US English Female",
+"sr" : "Serbian Male",
+"gd" : "US English Female",
+"sn" : "US English Female",
+"si" : "US English Female",
+"sk" : "Slovak Female",
+"sl" : "Slovak Female",
+"so" : "US English Female",
+"st" : "US English Female",
+"es" : "Spanish Female",
+"su" : "US English Female",
+"sw" : "Swahili Male",
+"ss" : "US English Female",
+"sv" : "Swedish Male",
+"ta" : "Tamil Male",
+"te" : "Tamil Male",
+"tg" : "US English Female",
+"th" : "Thai Female",
+"ti" : "US English Female",
+"bo" : "Chinese Female",
+"tk" : "Turkish Female",
+"tl" : "US English Female",
+"tn" : "US English Female",
+"to" : "US English Female",
+"tr" : "Turkish Female",
+"ts" : "US English Female",
+"tt" : "US English Female",
+"tw" : "US English Female",
+"ty" : "US English Female",
+"ug" : "Turkish Female",
+"uk" : "Russian Female",
+"ur" : "Hindi Female",
+"uz" : "Turkish Female",
+"ve" : "US English Female",
+"vi" : "Vietnamese Male",
+"vo" : "US English Female",
+"wa" : "US English Female",
+"cy" : "Welsh Male",
+"wo" : "US English Female",
+"fy" : "US English Female",
+"xh" : "US English Female",
+"yi" : "US English Female",
+"yo" : "US English Female",
+"za" : "US English Female",
+"zu" : "US English Female"
+}
 
 // Map Creation
 function initMap() {
@@ -84,6 +272,7 @@ function drawMap(data) {
         getCoordinates(event.latLng);
         showWindow();
         this.setOptions({fillOpacity: 0.25});
+
       });
 
       // Country hover out event.
@@ -95,6 +284,11 @@ function drawMap(data) {
       country.setMap(map);
     }
   }
+}
+
+//Play sound
+function playsound() {
+   responsiveVoice.speak(langData[hover_lang], jsonObj[hover_lang]);
 }
 
 // Get coordinates from mouse position.
@@ -125,7 +319,6 @@ inputButton.addEventListener('click', function() {
 
   inputText = inputTextBox.value;
   inputText = inputText.replace(' ', '+');
-  inputLang = inputLangBox.value;
 
   var url = "https://cors-anywhere.herokuapp.com/https://rede-182207.appspot.com/?lang=auto&text=" + inputText;
 
@@ -231,6 +424,7 @@ function showWindow() {
       var langUrl = "https://restcountries.eu/rest/v2/name/" + countryName;
       $.getJSON(langUrl, function (data1) {
         var lang = data1[0].languages[0].iso639_1;
+        hover_lang = lang;
         translate(countryName, lang);
       });
     });
@@ -243,6 +437,7 @@ function setWindow(content, position)
   infoWindow.setContent(content);
   infoWindow.setPosition(position);
   infoWindow.open(map);
+  setTimeout(playsound,3000)
 }
 
 // Hide window when hover out.
